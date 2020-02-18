2f5ea98febdb7a24a29bff3d0bb2fa708f625d77
chrpng <chrpng.brd@gmail.com>
2020-02-18 15:19:53 -0500

    Test API call in console with hidden key

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './reset.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cityInput: '',
+  }
+
+  fetchWeather = async (city) => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`, { mode: 'cors' });
+    if(response.ok) {
+      const json = await response.json();
+      console.log(json);
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.fetchWeather(this.state.cityInput)
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      cityInput: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Weather App</header>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange}/>
+          <button><i className="fas fa-search"></i></button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
