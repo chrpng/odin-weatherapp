e161f7bc69beb85e80486c0b68baa4db3370fe5a
chrpng <chrpng.brd@gmail.com>
2020-02-24 15:33:08 -0500

    Updates

---
+++ precache-manifest.cd54c7c88343a8773acc8d2c6230ebfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb29d2b045e7b273a78763cacfe2d1dc",
+    "url": "/odin-weatherapp/index.html"
+  },
+  {
+    "revision": "d66ea9bb4ba959935462",
+    "url": "/odin-weatherapp/static/css/main.d3576328.chunk.css"
+  },
+  {
+    "revision": "84fc907e2ae1e1e57745",
+    "url": "/odin-weatherapp/static/js/2.41b33ec3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/odin-weatherapp/static/js/2.41b33ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66ea9bb4ba959935462",
+    "url": "/odin-weatherapp/static/js/main.984c06bf.chunk.js"
+  },
+  {
+    "revision": "64d1ec305bb299186a9c",
+    "url": "/odin-weatherapp/static/js/runtime-main.bb9a4aea.js"
+  }
+]);
